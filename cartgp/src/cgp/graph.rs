4ec0bef455d7da133ea833f3b5d355e6bac8d9df
thegoldencanary <thegoldencanary8@gmail.com>
2017-05-10 00:04:35 +1200

    Added basic structure of graph and nodes

---
+++ cartgp/src/cgp/graph.rs
@@ -0,0 +1,32 @@
+
+
+// The Triple struct representing a gene
+// Contains a function, and two input indexes.
+struct Triple {
+
+	function: fn(i64,i64),
+	input_one: i64,
+	input_two: i64,
+
+}
+
+// The Graph struct containing the list of genes
+// and the input into the Graph, and a list of 
+// functions to be used as nodes
+struct Graph {
+
+	input: Vec<i64>,
+	genome: Vec<Triple>,
+	nodes: Vec<fn(i64,i64)>,
+
+}
+
+impl Triple {
+
+
+}
+
+impl Graph {
+
+
+}
