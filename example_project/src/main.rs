9b59e9324dc432c97d95eaf83a5b1a3017187e27
thegoldencanary <thegoldencanary8@gmail.com>
2017-06-08 16:09:32 +1200

    Example stuff

--- example_project/src/main.rs
+++ example_project/src/main.rs
@@ -1,5 +1,19 @@
 extern crate cgp_rs;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to cgp_rs!");
+	
+	/*
+	
+		The following example shows how to use cgp_rs.
+		
+		cgp_rs's main functionality is given by the cgp_rs::graph::Graph struct.
+		This struct contains the population of genomes that are used in the algorithm.
+		
+		
+	
+	*/
+	
+	
+	
 }
